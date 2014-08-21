c66a7159ee501891e0ae7362e23353a120cab605
alanmeyer <alanmeyer@yahoo.com>
2014-08-20 18:18:30 -0700

    Commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# postinstall
+#### Alan Meyer
+##### https://github.com/alanmeyer/postinstall-config
+   
+###### Instructions   
+   
+(1) Reset VPS to Ubuntu 14.04 LTS & set desired root password (if required)
+```
+passwd
+```
+
+(2) Install python
+```
+apt-get update > /tmp/python.log
+apt-get -y install python >> /tmp/python.log
+```
+
+(3) Run postinstall
+```
+echo 'check_certificate = off' | tee $HOME/.wgetrc
+mkdir -p ~/scripts/postinstall
+cd ~/scripts/postinstall
+wget http://raw.github.com/alanmeyer/postinstall-config/mserver/postinstall_config.sh -O postinstall_config.sh
+chmod +x *.sh
+./postinstall_config.sh
+```
+
+(4) Set administrator password and lock out ssh to other groups
+```
+passwd administrator
+echo "AllowGroups sshusers" | tee -a /etc/ssh/sshd_config
+```
+
+(5) Setup Apache2 SSL
+```
+./apache2_ssl_config.sh
+```
+
+(6) Setup MySQL
+```
+mysql_secure_installation
+```
+- n -> y -> <password>
+- y -> y -> y -> y
+
+(7) Setup phpmyadmin
+```
+dpkg-reconfigure phpmyadmin
+```
+- Reinstall db:             Yes
+- Connection method:        unix socket
+- Administrator:            root
+- Password:                 <password-from-mysql-setup>
+- MySQL User:               phpmyadmin
+- db Name:                  phpmyadmin
+- Webserver to reconfig:    apache2
+
+(8) Reboot
+```
+reboot
+```
---
+++ postinstall_config.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+# Alan Meyer
+# https://github.com/alanmeyer/postinstall-config
+
+# Server Properties
+# Note: mail can change based on if this is the primary domain mail server or not
+#       e.g. primary:   %(domain)s
+#            secondary: %(hostname)s.%(domain)s
+GIT_POSTINSTALL=https://raw.github.com/alanmeyer/postinstall/master
+GIT_POSTINSTALL_CFG=https://raw.github.com/alanmeyer/postinstall-config/mserver
+GIT_SCRIPT=postinstall
+OS_VERSION=trusty
+IP=107.150.5.22
+HOSTNAME=mserver
+DOMAIN=ocmeyer.com
+
+# Script files
+GIT_SCRIPT_PY=$GIT_SCRIPT".py"
+GIT_SCRIPT_CFG=$GIT_SCRIPT".cfg"
+GIT_SCRIPT_LOG=$GIT_SCRIPT".log"
+GIT_SCRIPT_A2SSL=apache2_ssl_config.sh
+
+# Remove any existing script & log
+rm -f $GIT_SCRIPT_CFG $GIT_SCRIPT_LOG
+
+# Get the necessary files
+wget $GIT_POSTINSTALL/$GIT_SCRIPT_PY        -O $GIT_SCRIPT_PY
+wget $GIT_POSTINSTALL/$GIT_SCRIPT_A2SSL     -O $GIT_SCRIPT_A2SSL
+wget $GIT_POSTINSTALL_CFG/$GIT_SCRIPT_CFG   -O $GIT_SCRIPT_CFG
+
+# Update our config file to reflect our specific git project
+sed -i "s,^\(postinstall             = \).*,\1$GIT_POSTINSTALL,g"       $GIT_SCRIPT_CFG
+sed -i "s,^\(postinstall_config      = \).*,\1$GIT_POSTINSTALL_CFG,g"   $GIT_SCRIPT_CFG
+sed -i "s/^\(os_version              = \).*/\1$OS_VERSION/g"            $GIT_SCRIPT_CFG
+sed -i "s/^\(ip                      = \).*/\1$IP/g"                    $GIT_SCRIPT_CFG
+sed -i "s/^\(hostname                = \).*/\1$HOSTNAME/g"              $GIT_SCRIPT_CFG
+sed -i "s/^\(domain                  = \).*/\1$DOMAIN/g"                $GIT_SCRIPT_CFG
+
+# Update Apache SSL config script
+sed -i "s,^\(GIT_SHARE\).*,\1=$GIT_POSTINSTALL_CFG,g"                   $GIT_SCRIPT_A2SSL
+
+# Change to executable
+chmod +x *.sh
+
+# Run our script with our configuration
+python $GIT_SCRIPT_PY -c $GIT_SCRIPT_CFG
